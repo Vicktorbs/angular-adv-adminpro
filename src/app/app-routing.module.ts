8431f8c8a70ae00bc63a2e96f83ba5fa78e21f4f
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-08-06 18:03:37 -0500

    Multiple modules creation, new structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
 import { PagesComponent } from './pages/pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
