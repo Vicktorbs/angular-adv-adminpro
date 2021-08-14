e069b11680033d092af1934e15c1d06e90d5ddec
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-08-13 23:31:16 -0500

    Pluggins initialization

--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from 'src/app/services/settings.service';
 
+declare function customInitFunctions();
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -11,7 +13,7 @@ export class PagesComponent implements OnInit {
   constructor(private settingServices: SettingsService) { }
 
   ngOnInit(): void {
-    
+    customInitFunctions();
   }
 
 }
