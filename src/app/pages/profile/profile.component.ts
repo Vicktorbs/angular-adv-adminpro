986dd9b68773cc0119a4ad4be5b27487014d4469
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-08-26 21:43:03 -0500

    Update user imagen

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/user.model';
+import { FileUploadService } from 'src/app/services/file-upload.service';
 import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-profile',
@@ -10,9 +13,19 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfileComponent implements OnInit {
 
   public profileForm: FormGroup;
+  public imagenToUpload: File;
+  public imgTemp: any = null;
+  public userImg: any = null;
 
   constructor(private fb: FormBuilder,
-              private userService: UserService) { }
+              private userService: UserService,
+              private fileUploadService: FileUploadService) {
+    this.userImg = {
+      img: userService.user.imageUrl,
+      google: userService.user.google
+    }
+    
+  }
 
   ngOnInit(): void {
     this.profileForm = this.fb.group({
@@ -28,6 +41,36 @@ export class ProfileComponent implements OnInit {
       const { name, email } = this.profileForm.value
       this.userService.user.name = name;
       this.userService.user.email = email;
+
+      Swal.fire('Saved', 'Changes saved', 'success');
+    }, (err) => {
+      Swal.fire('Error', err.error.msg, 'error');
+    })
+  }
+
+  changeImagen(file: File) {
+    this.imagenToUpload = file;
+    
+    if (!file) {
+      return this.imgTemp = null
+    }
+    const reader = new FileReader();
+    
+    reader.onloadend = () => {
+      this.imgTemp  = reader.result;
+    }
+    reader.readAsDataURL(this.imagenToUpload)
+    
+  }
+
+  uploadImagen() {
+    this.fileUploadService.updatePhoto(this.imagenToUpload, 'users', this.userService.user.uid).then(
+      img => {
+        this.userService.user.img = img;
+        Swal.fire('Saved', 'Imagen saved', 'success');
+      }
+    ).catch(err => {
+      Swal.fire('Error', 'Imagen can not be saved', 'error');
     })
   }
 
