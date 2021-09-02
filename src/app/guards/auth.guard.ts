b0ebdca2300ce194437b9e0cfff806f74d782c1a
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-09-02 17:57:36 -0500

    Lazy load - env.prod

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,27 +1,40 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanLoad, Route, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private userService: UserService,
               private router: Router) {}
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+    return this.userService.validateToken().pipe(
+      tap(isAtutenticated => {
+        if (!isAtutenticated) {
+          this.router.navigateByUrl('/login')
+        }
+      })
+    );
+
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
 
-      return this.userService.validateToken().pipe(
-        tap(isAtutenticated => {
-          if (!isAtutenticated) {
-            this.router.navigateByUrl('/login')
-          }
-        })
-      );
+    return this.userService.validateToken().pipe(
+      tap(isAtutenticated => {
+        if (!isAtutenticated) {
+          this.router.navigateByUrl('/login')
+        }
+      })
+    );
 
   }
   
